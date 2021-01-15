a14c64593b5fab97032434315867c11769f8ac98
bluescurry <18832643723@163.com>
2021-01-15 23:31:49 +0800

    2.3.0

--- src/getScriptQuery.ts
+++ src/getScriptQuery.ts
@@ -9,7 +9,6 @@ const getScriptQuery = (field: string, defaultValue: string | null = null ): str
   // 启动命令拆分对象
   const argv = JSON.parse(process.env.npm_config_argv || '{}');
   if (argv.original) {
-    // eslint-disable-next-line no-bitwise
     let fieldValue = argv.original.find((item: string): number => ~item.indexOf(`--${field}=`)) || defaultValue;
 
     if (fieldValue) {
